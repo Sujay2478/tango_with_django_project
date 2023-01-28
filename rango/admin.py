cd9dead382e922517bd16149d6c813b3db352245
“Sujay <2672826P@student.gla.ac.uk>
2023-01-28 03:06:38 +0000

    Chapter 6

--- rango/admin.py
+++ rango/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from tango_with_django_project.rango.models import Category, Page
+from rango.models import Category, Page
 
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
 
 admin.site.register(Category)
 admin.site.register(Page, PageAdmin)
