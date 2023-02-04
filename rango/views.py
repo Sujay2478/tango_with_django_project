f08b5acdac09ba7ec93838e91a10a7f94bbf0ef9
“Sujay <2672826P@student.gla.ac.uk>
2023-02-04 02:24:49 +0000

    Chapter 10

--- rango/views.py
+++ rango/views.py
@@ -9,6 +9,7 @@ from django.shortcuts import redirect
 from django.urls import reverse
 from rango.forms import PageForm
 from rango.forms import UserForm, UserProfileForm
+from datetime import datetime
 
 
 def index(request):
@@ -21,12 +22,16 @@ def index(request):
     context_dict['pages'] = page_list
     context_dict['extra'] = 'From the model solution on GitHub'
 
-    return render(request, 'rango/index.html', context=context_dict)
+    visitor_cookie_handler(request)
+
+    response = render(request, 'rango/index.html', context=context_dict)
+    return response
 
 
 def about(request):
-    # Spoiler: you don't need to pass a context dictionary here.
-    return render(request, 'rango/about.html')
+    context_dict = {}
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
 
 
 def show_category(request, category_name_slug):
@@ -147,3 +152,23 @@ def restricted(request):
 def user_logout(request):
     logout(request)
     return redirect(reverse('rango:index'))
+
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+
+    request.session['visits'] = visits
